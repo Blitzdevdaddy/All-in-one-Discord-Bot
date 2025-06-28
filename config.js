1cecdea9d5ac7c142870afa71d9b2328272434d9
Blitz <86040000+Blitzdevdaddy@users.noreply.github.com>
2025-06-29 02:16:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-exports.TOKEN = "ODg3MDI3NzgzMTI4MjE5NjY4.YT-K_w.Ilzs2NEqqOGv_psgrZQVcqmu2Oc";
+exports.TOKEN = "";
 
 exports.DBL_API_KEY = "NONE";
 
 exports.PREFIX = ">";
 
-exports.GOOGLE_API_KEY = "AIzaSyDzKnUyDiDaZru7_zbAkPbIdqhhnjLvd8U";
+exports.GOOGLE_API_KEY = "";
 
-exports.GENIUS_API_KEY = "VE-Hkn049y3FN2oYctEIL1Kc6eCaORIrNNaPXKqihSHTJqWjnvh4FFm67Upgo5Jj";
+exports.GENIUS_API_KEY = "";
 
-exports.yandex_API = "trnsl.1.1.20200524T161905Z.bfe2aa5a35ce8bd9.2e55fde04fbce222eb14506eba89f1dbfcfc973d";
+exports.yandex_API = "";
 
-exports.news_API = "ac620cf6a0aa49b398f8f071edcaa71f";
+exports.news_API = "";
 
-exports.giphy_API = "RmKm0HixW7lQ6F8cTSQpyWaj0bjT7laH";
+exports.giphy_API = "";
 
-exports.AME_API = "a0f7f1b61e152b375952b83d2b6a7ae64c32fdc2fcc7a4f6b6963dc4c447731745c451adabde6a070731bb99739c68350668074b7fcec856e8222a75ab7da555";
+exports.AME_API = "";
 
-exports.blague_API = "r.00yqXn3KtkY41GNdGCB._JbaMPdqyAyLA6JLkCjftATM5QXTvdatalXNzvcBR-";
+exports.blague_API = "";
